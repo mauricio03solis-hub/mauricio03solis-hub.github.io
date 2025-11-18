19470cc4fd2eece51917dde9770b8d3e657e22be
Mauricio <mauricio03solis@gmail.com>
2025-11-17 21:31:50 -0600

    first commit

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,8 @@
+
+import __ASTRO_IMAGE_IMPORT_ZTTuGd from "../../assets/music.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
+import __ASTRO_IMAGE_IMPORT_12tEC2 from "../../assets/bici.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md";
+import __ASTRO_IMAGE_IMPORT_Z2fVq7D from "../../assets/campo.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond-post.md";
+import __ASTRO_IMAGE_IMPORT_1xM32I from "../../assets/salida.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthird-post.md";
+import __ASTRO_IMAGE_IMPORT_Z1ubb6F from "../../assets/viaje.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
+export default new Map([["../../assets/music.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZTTuGd], ["../../assets/bici.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md", __ASTRO_IMAGE_IMPORT_12tEC2], ["../../assets/campo.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsecond-post.md", __ASTRO_IMAGE_IMPORT_Z2fVq7D], ["../../assets/salida.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthird-post.md", __ASTRO_IMAGE_IMPORT_1xM32I], ["../../assets/viaje.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_Z1ubb6F]]);
+		
